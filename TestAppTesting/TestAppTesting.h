0625bcff7bd6ae1072fadbccb53ab093f073716a
kneeloy <kneeloy@gmail.com>
2013-01-30 12:03:56 +0530

    TestBundle

---
+++ TestAppTesting/TestAppTesting.h
@@ -0,0 +1,13 @@
+//
+//  TestAppTesting.h
+//  TestAppTesting
+//
+//  Created by COGNIZANT PERSONAL on 1/30/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestAppTesting : SenTestCase
+
+@end
---
+++ TestAppTesting/TestAppTesting.m
@@ -0,0 +1,32 @@
+//
+//  TestAppTesting.m
+//  TestAppTesting
+//
+//  Created by COGNIZANT PERSONAL on 1/30/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestAppTesting.h"
+
+@implementation TestAppTesting
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestAppTesting");
+}
+
+@end
